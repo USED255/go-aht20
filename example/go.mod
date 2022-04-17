3432859d8ef182110173107db7d9965143600094
我如天翼 <wrtyis@outlook.com>
2022-04-17 16:21:28 +0800

    更新

---
+++ example/go.mod
@@ -0,0 +1,11 @@
+module aht20-test
+
+go 1.17
+
+require (
+	github.com/d2r2/go-i2c v0.0.0-20191123181816-73a8a799d6bc
+	github.com/d2r2/go-logger v0.0.0-20210606094344-60e9d1233e22
+	github.com/used255/aht20-go v0.0.0-20220417081017-dcf6b5cba472
+)
+
+require github.com/davecgh/go-spew v1.1.1 // indirect
---
+++ example/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/d2r2/go-i2c"
+	logger "github.com/d2r2/go-logger"
+
+	aht20 "github.com/used255/aht20-go"
+)
+
+func main() {
+	logger.ChangePackageLogLevel("i2c", logger.InfoLevel)
+	bus, err := i2c.NewI2C(0x38, 1)
+	if err != nil {
+		return
+	}
+	aht20 := aht20.AHT20New(bus)
+	err = aht20.ReadWithRetry(3)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("温度:", aht20.Celsius(), "摄氏度")
+	fmt.Println("相对湿度:", aht20.RelHumidity(), "%")
+}
