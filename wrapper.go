fcefce6a5ed5aa219a77d92ab006ff520bcfe23e
我如天翼 <wrtyis@outlook.com>
2025-07-26 23:04:01 +0800

    粘贴License

--- wrapper.go
+++ wrapper.go
@@ -39,15 +39,6 @@ func (d *Device) tx(w, r []byte) error {
 	return nil
 }
 
-// 来自 https://github.com/d2r2/go-bh1750/blob/master/logger.go
-
-// You can manage verbosity of log output
-// in the package by changing last parameter value.
-var lg = logger.NewPackageLogger("aht20",
-	logger.DebugLevel,
-	// logger.InfoLevel,
-)
-
 // New creates a new AHT20 connection. The I2C bus must already be configured.
 func NewAHT20(bus *i2c.I2C) Device {
 	aht20 := New(bus)
@@ -72,3 +63,34 @@ func (d *Device) ReadCelsius() (float32, error) {
 	}
 	return d.Celsius(), nil
 }
+
+// 来自  https://github.com/d2r2/go-bh1750/blob/master/logger.go
+// 许可: https://github.com/d2r2/go-bh1750/blob/master/LICENSE
+/* MIT License
+
+Copyright (c) 2018 Denis Dyakov
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE. */
+
+// You can manage verbosity of log output
+// in the package by changing last parameter value.
+var lg = logger.NewPackageLogger("aht20",
+	logger.DebugLevel,
+	// logger.InfoLevel,
+)
